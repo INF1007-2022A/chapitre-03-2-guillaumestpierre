ceef668efd58b753c588f85fd2745611ddda1d1f
guillaumestpierre <113061567+guillaumestpierre@users.noreply.github.com>
2022-09-09 17:19:15 -0400

    exercices 3.2
    J'ai fait les 4 premiers exercices.

--- exercice.py
+++ exercice.py
@@ -3,32 +3,50 @@
 
 def dissipated_power(voltage, resistance):
 	# TODO: Calculer la puissance dissipée par la résistance.
-	return 0
+	return voltage*voltage/resistance
 
 def orthogonal(v1, v2):
 	# TODO: Retourner vrai si les vecteurs sont orthogonaux, faux sinon.
 	v1[0] # Pour accéder au X
 	v1[1] # Pour accéder au Y
-	pass
+	o = v1[0]*v2[0]+v1[1]*v2[1]
+	if (o == 0):
+			return('true')
+	else:
+			return('false')
 
 def average(values):
 	# TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
+	m = 0
+	t = 0
 	for v in values:
-		pass # La variable v contient une valeur de la liste.
+		if (0 <= v):
+				m += v
+				t += 1
+	return (m/t)
 
 def bills(value):
 	# TODO: Calculez le nombre de billets de 20$, 10$ et 5$ et pièces de 1$ à remettre pour représenter la valeur.
+	twenties = 0
+	tens = 0
+	fives = 0
+	twos = 0
+	ones = 0
 	while value != 0:
 		if value >= 20:
-			pass
+			twenties = twenties + 1
+			value = value - 20
 		elif value >= 10:
-			pass
+			tens = tens + 1
+			value = value - 10
 		elif value >= 5:
-			pass
+			fives = fives + 1
+			value = value - 5
 		elif value >= 1:
-			pass
+			ones = ones + 1
+			value = value - 1
 
-	return (twenties, tens, fives, twos, ones);
+	print (twenties, tens, fives, twos, ones);
 
 def format_base(value, base, digit_letters):
 	# Formater un nombre dans une base donné en utilisant les lettres fournies pour les chiffres<
